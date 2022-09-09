3807ee224ef9845252b447a43f3a634fb4a900d5
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-09-09 19:38:29 +0530

    initial changes

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Container'>
+        <div className="carding">
+            <h1>Onkar Deoalankar</h1>
+            <h2>Full Stack Web Developer</h2>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
