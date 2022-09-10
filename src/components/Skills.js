1f1a55570351ba287690784c6e956c04fc662a2b
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-09-10 18:01:56 +0530

    About Us updated

---
+++ src/components/Skills.js
@@ -0,0 +1,22 @@
+import React from "react";
+import html from '../icons/html.png';
+import css from '../icons/css.png';
+import javascript from '../icons/javascript.png';
+import bootstrap from '../icons/bootstrap.png';
+import react from '../icons/react.png';
+import mysql from '../icons/mysql.png';
+import nodejs from '../icons/nodejs.png';
+import mongodb from '../icons/mongodb.png';
+import express from '../icons/express.png';
+import aws from '../icons/aws.png';
+import git from '../icons/git.png';
+import postman from '../icons/postman.png';
+
+const Skills = () => {
+	return 
+    <>
+        
+    </>;
+};
+
+export default Skills;
