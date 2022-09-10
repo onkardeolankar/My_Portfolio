e0dc38222451904bc264ddbc1c32638489d8547a
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-09-11 01:55:40 +0530

    css file updated

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,7 +4,7 @@ const AboutUs = () => {
   return (
     <>
         <div className='about-details'>
-            <h2>About</h2>
+            <h2>About</h2><hr />
             <h4>Software Developer with 0.7yr of experience in OnBase Technology facilitating cutting edge engineering solutions.
             My goal is to be the best web developer that I can be and to contribute to the technology industry all that I know and can do. I am dedicated to 
       perfecting my craft by learning from more seasoned developers, remaining humble, and continuously making strides to learn all that i can about development.
